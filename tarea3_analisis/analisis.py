f51d5bf24b7fd8a531e051debc55f46116e13a54
sofiaarangoaz <s.arango112@uniandes.edu.co>
2026-09-13 09:23:37 -0500

    Cambios y arreglos en el análisis

--- tarea3_analisis/analisis.py
+++ tarea3_analisis/analisis.py
@@ -1,4 +1,5 @@
-
+######################################################################
+#Sofía
 #Importar librerías necesarias
 import pandas as pd
 import numpy as np
@@ -8,50 +9,23 @@ from scipy import stats
 import seaborn as sns
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
-
-#cargar los datos
-df = pd.read_csv("tarea2_ingenieria_datos/Datos_MinisterioHacienda.csv")
+print("-------------------------------------------------------------------------------------------------------------")
+print("Análisis Sofía - ¿Cuánta plata contrató el Ministerio cada año y en qué tipo de contratos se concentra?:")
+print("-------------------------------------------------------------------------------------------------------------")
+#Cargar los datos
+df = pd.read_csv("../tarea2_ingenieria_datos/datos_limpios.csv")
 
 print("Dimensión de los datos originales:")
 print(df.shape)
 
-
-#Preparar las variables necesarias para responder la pregunta: ¿Cuánta plata contrató el Ministerio cada año, 
-# y en qué tipo de contratos se concentra?
-df = df[df["valor_del_contrato"] < 1e12].copy()
-
-#Convertir las fechas a formato datetime
-df["fecha_de_firma"] = pd.to_datetime(df["fecha_de_firma"],errors="coerce")
-
-df["fecha_de_inicio_del_contrato"] = pd.to_datetime(df["fecha_de_inicio_del_contrato"],errors="coerce")
-
-#Usar la fecha de firma y, si no existe, la fecha de inicio
-df["fecha_analisis"] = df["fecha_de_firma"].fillna(df["fecha_de_inicio_del_contrato"])
-
-# Crear variable año
-df["anio"] = df["fecha_analisis"].dt.year
-
-# Organizar los nombres de los tipos de contrato
-df["tipo_de_contrato"] = (df["tipo_de_contrato"].str.strip().str.capitalize())
-
-# Conservar los contratos que tienen un año identificado
-df_analisis = df[df["anio"].notna()].copy()
-
-print("\nDimensión de los datos utilizados en el análisis:")
-print(df_analisis.shape)
-
-
-
-
-#Se hace un análisis descriptivo de la variable valor_del_contrato, 
+#Se hace un análisis descriptivo de la variable valor_del_contrato 
 print("\nEstadísticas descriptivas del valor de los contratos:")
-print(df_analisis["valor_del_contrato"].describe())
+print(df["valor_del_contrato"].describe())
 
 #Histograma para visualizar la distribución original del valor de los contratos
 plt.figure(figsize=(10, 6))
-
 plt.hist(
-    df_analisis["valor_del_contrato"],
+    df["valor_del_contrato"],
     bins=30,
     edgecolor="black"
 )
@@ -67,7 +41,7 @@ plt.show()
 plt.figure(figsize=(10, 6))
 
 plt.hist(
-    np.log1p(df_analisis["valor_del_contrato"]),
+    np.log1p(df["valor_del_contrato"]),
     bins=30,
     edgecolor="black"
 )
@@ -84,7 +58,7 @@ plt.show()
 plt.figure(figsize=(10, 5))
 
 plt.boxplot(
-    np.log1p(df_analisis["valor_del_contrato"]),
+    np.log1p(df["valor_del_contrato"]),
     vert=False
 )
 
@@ -96,15 +70,15 @@ plt.show()
 
 
 #Ahora se busca saber cuanta plata contrató el ministerio cada año, 
-# para esto se agrupa por cada año y de ahí calculan estadísticas descriptivas del valor de los contratos.
+#para esto se agrupa por cada año y de ahí calculan estadísticas descriptivas del valor de los contratos.
 resumen_anual = (
-    df_analisis
+    df
     .groupby("anio")["valor_del_contrato"]
     .agg(["count", "sum", "mean", "median", "max"])
     .reset_index()
 )
 
-# Cambiar nombres de columnas
+#Cambiar nombres de columnas
 resumen_anual.columns = [
     "anio",
     "numero_contratos",
@@ -116,7 +90,7 @@ resumen_anual.columns = [
 print("\nResumen de contratación por año:")
 print(resumen_anual)
 
-#visualización de cuánto dinero se contrató en cada año
+#Visualización de cuánto dinero se contrató en cada año
 plt.figure(figsize=(11, 6))
 
 plt.bar(
@@ -135,9 +109,9 @@ plt.show()
 
 
 #Se analizaron en que tipo de contratos se concentra el dinero contratado por el ministerio, 
-# Se agrupa por tipo de contrato y se calculan estadísticas descriptivas del valor de los contratos.
+#Se agrupa por tipo de contrato y se calculan estadísticas descriptivas del valor de los contratos.
 resumen_tipo = (
-    df_analisis
+    df
     .groupby("tipo_de_contrato")["valor_del_contrato"]
     .agg(["count", "sum", "mean", "median"])
     .reset_index()
@@ -151,21 +125,21 @@ resumen_tipo.columns = [
     "valor_mediano"
 ]
 
-# Porcentaje del valor total
+#Porcentaje del valor total
 resumen_tipo["porcentaje_valor"] = (
     resumen_tipo["valor_total"]
     / resumen_tipo["valor_total"].sum()
     * 100
 )
 
-# Porcentaje de contratos
+#Porcentaje de contratos
 resumen_tipo["porcentaje_contratos"] = (
     resumen_tipo["numero_contratos"]
     / resumen_tipo["numero_contratos"].sum()
     * 100
 )
 
-# Ordenar de mayor a menor valor contratado
+#Ordenar de mayor a menor valor contratado
 resumen_tipo = resumen_tipo.sort_values(
     "valor_total",
     ascending=False
@@ -175,9 +149,7 @@ print("\nContratación por tipo de contrato:")
 print(resumen_tipo.to_string(index=False))
 
 
-
-
-#visualización de qué tipos de contrato concentran mayor cantidad de dinero
+#Visualización de qué tipos de contrato concentran mayor cantidad de dinero
 datos_tipo = resumen_tipo.sort_values("valor_total")
 
 plt.figure(figsize=(11, 7))
@@ -198,7 +170,7 @@ plt.show()
 #Relación entre año y tipo de contrato, 
 #Se agrupa por año y tipo de contrato y se calcula el valor total contratado.
 resumen_anio_tipo = (
-    df_analisis
+    df
     .groupby(
         ["anio", "tipo_de_contrato"]
     )["valor_del_contrato"]
@@ -210,7 +182,7 @@ print("\nValor contratado por año y tipo de contrato:")
 print(resumen_anio_tipo.to_string(index=False))
 
 
-# Mostrar los registros con mayor valor para cada año
+#Mostrar los registros con mayor valor para cada año
 resumen_ordenado = resumen_anio_tipo.sort_values(
     ["anio", "valor_del_contrato"],
     ascending=[True, False]
@@ -223,10 +195,10 @@ tipo_principal_anio = resumen_ordenado.drop_duplicates(
 print("\nTipo de contrato con mayor valor en cada año:")
 print(tipo_principal_anio.to_string(index=False))
 
-# Seleccionar los 6 tipos de contrato con mayor valor total
-# y crear tabla para comparar el valor contratado por año y tipo de contrato
+#Seleccionar los 6 tipos de contrato con mayor valor total
+#y crear tabla para comparar el valor contratado por año y tipo de contrato
 principales = (
-    df_analisis
+    df
     .groupby("tipo_de_contrato")["valor_del_contrato"]
     .sum()
     .sort_values(ascending=False)
@@ -234,8 +206,8 @@ principales = (
     .index
 )
 
-datos_mapa = df_analisis[
-    df_analisis["tipo_de_contrato"].isin(principales)
+datos_mapa = df[
+    df["tipo_de_contrato"].isin(principales)
 ]
 
 tabla = datos_mapa.pivot_table(
@@ -246,7 +218,7 @@ tabla = datos_mapa.pivot_table(
     fill_value=0
 )
 
-# Convertir a miles de millones de pesos
+#Convertir a miles de millones de pesos
 tabla = tabla / 1e9
 
 plt.figure(figsize=(12, 6))
@@ -277,16 +249,16 @@ plt.ylabel("Tipo de contrato")
 plt.tight_layout()
 plt.show()
 
-# Diagrama de violin para comparar la distribución del valor de los contratos
-# para los tipos más frecuentes
+#Diagrama de violin para comparar la distribución del valor de los contratos
+#para los tipos más frecuentes
 
-tipos = df_analisis["tipo_de_contrato"].value_counts().head(5).index
+tipos = df["tipo_de_contrato"].value_counts().head(5).index
 
 datos = []
 
 for tipo in tipos:
-    valores = df_analisis[
-        df_analisis["tipo_de_contrato"] == tipo
+    valores = df[
+        df["tipo_de_contrato"] == tipo
     ]["valor_del_contrato"]
 
     datos.append(np.log1p(valores))
@@ -309,13 +281,13 @@ plt.tight_layout()
 plt.show()
 
 
-# Diagrama de dispersión para ver la relación entre el año y el valor de los contratos
+#Diagrama de dispersión para ver la relación entre el año y el valor de los contratos
 
 plt.figure(figsize=(10, 6))
 
 plt.scatter(
-    df_analisis["anio"],
-    np.log1p(df_analisis["valor_del_contrato"]),
+    df["anio"],
+    np.log1p(df["valor_del_contrato"]),
     alpha=0.4
 )
 
@@ -327,11 +299,11 @@ plt.tight_layout()
 plt.show()
 
 
-# Correlación entre el año y el valor del contrato
+#Correlación entre el año y el valor del contrato
 
 correlacion, p_valor = pearsonr(
-    df_analisis["anio"],
-    df_analisis["valor_del_contrato"]
+    df["anio"],
+    df["valor_del_contrato"]
 )
 
 print("\nCorrelación entre año y valor del contrato:")
@@ -342,6 +314,11 @@ if p_valor < 0.05:
 else:
     print("No existe una relación significativa entre el año y el valor de los contratos.")
 
+
+######################################################################
+print("-----------------------------------------------------------------------------------------------------------------------------------")
+print("Análisis Juan - ¿Difiere el porcentaje de ejecución presupuestal (pagado / contratado) al momento del corte según si el contratista es PyME, consorcio, o el género de su representante legal?:")
+print("-----------------------------------------------------------------------------------------------------------------------------------")
 #JSQO
 # % de ejecución = pagado / contratado * 100 (excluye contratos con valor 0)
 valor_valido = df["valor_del_contrato"].where(~df["valor_contrato_cero"], np.nan)
